7e866f8251cc8dedeac70656c02b118378e7b8a9
Dina <vitor.dhers@gmail.com>
2025-03-18 05:20:28 -0300

    updates scripts for cloning metaplex program from mainnet

--- scripts.sh
+++ scripts.sh
@@ -1,8 +1,34 @@
 #! /bin/bash
 
 SOLANA_PROGRAMS=("hello_solana" "calculator" "transfer_sol")
+CLONE_UPGRADEABLE_PROGRAMS=("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s")
 
 case $1 in
+    "start")
+        CMD="solana-test-validator"
+        if [[ "$2" == "--clone-programs" ]]; then
+          echo "Cloning programs:"
+            for program in "${CLONE_UPGRADEABLE_PROGRAMS[@]}"; do
+                echo " - $program"
+                CMD+=" --clone-upgradeable-program $program"
+            done
+            CMD+=" --url https://api.mainnet-beta.solana.com"
+        fi
+        eval "$CMD"
+        ;;
+    "start-new")
+        CMD="solana-test-validator"
+        if [[ "$2" == "--clone-programs" ]]; then
+          echo "Cloning programs:"
+            for program in "${CLONE_UPGRADEABLE_PROGRAMS[@]}"; do
+                echo " - $program"
+                CMD+=" --clone-upgradeable-program $program"
+            done
+            CMD+=" --url https://api.mainnet-beta.solana.com"
+        fi
+        CMD+=" --reset"
+        eval "$CMD"
+        ;;
     "reset")
         for x in $(solana program-v4 show | awk 'RP==0 {print $1}'); do 
             if [[ $x != "Program" ]]; 
