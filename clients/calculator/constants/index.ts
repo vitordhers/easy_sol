f18afdf5f27e1cfcac967ecd68fed3295c3edbd6
Dina <vitor.dhers@gmail.com>
2025-03-22 15:09:05 -0300

    adds static serialization helper

--- clients/calculator/constants/index.ts
+++
@@ -1,8 +0,0 @@
-import { serialize } from "npm:@dao-xyz/borsh";
-import { CalculatorInstruction, CalculatorSchema } from "../models/index.ts";
-
-export const CALCULATOR_SIZE = serialize(new CalculatorSchema()).length;
-
-export const CALCULATOR_INSTRUCTIONS_SIZE = serialize(
-  new CalculatorInstruction(),
-).length;
--- shared/models.ts
+++ shared/models.ts
@@ -1,7 +1,27 @@
-import { serialize } from "npm:@dao-xyz/borsh";
+import { Buffer } from "node:buffer";
+import {
+  AbstractType,
+  Constructor,
+  deserialize,
+  serialize,
+} from "npm:@dao-xyz/borsh";
 
-export abstract class SerializableSchema {
-  serialize() {
-    return serialize(this);
+export abstract class SerializationHelper {
+  private constructor() {
+    throw new Error("StupidMath is a static class and cannot be instantiated!");
+  }
+  static serialize<T>(schema: T): Buffer {
+    return Buffer.from(serialize(schema));
+  }
+
+  static getDataSize<T>(schema: T): number {
+    return SerializationHelper.serialize(schema).length;
+  }
+
+  static deserialize<T>(
+    buffer: Buffer,
+    ctor: Constructor<T> | AbstractType<T>,
+  ): T {
+    return deserialize(buffer, ctor);
   }
 }
